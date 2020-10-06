ca33d658566df4b7f66ac2de5ca649d18a3dea13
Vladimir Erdman <pm@vladimirerdman.com>
2020-10-07 02:23:48 +0300

    homework 3

--- netty-server/src/main/java/Server.java
+++ netty-server/src/main/java/Server.java
@@ -1,2 +1,49 @@
+import examples.StringHandler;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.serialization.ClassResolvers;
+import io.netty.handler.codec.serialization.ObjectDecoder;
+import io.netty.handler.codec.serialization.ObjectEncoder;
+
 public class Server {
+    private static int PORT = 8189;
+
+    public Server(int PORT) { Server.PORT = PORT; }
+
+    Server() {
+        EventLoopGroup authGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap sb = new ServerBootstrap();
+            sb.group(authGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            socketChannel.pipeline().addLast(
+                                    new ObjectEncoder(),
+                                    new ObjectDecoder(ClassResolvers.cacheDisabled(null)),
+                                    new StringHandler()
+                            );
+                        }
+                    });
+            System.out.println("Server started");
+            ChannelFuture future = sb.bind(PORT).sync();
+            future.channel().closeFuture().sync();
+            System.out.println("Server closed");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            authGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) {
+        new Server();
+    }
 }
